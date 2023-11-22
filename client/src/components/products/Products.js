c00c27d768ed672ed3de93ce208e185056434771
Svitlana <Svetlanalynne@gmail.com>
2023-11-22 16:29:51 -0400

    checkboxes in progress

--- client/src/components/products/Products.js
+++ client/src/components/products/Products.js
@@ -3,8 +3,10 @@ import ImportBar from "../import-Bar";
 
 function Products() {
   const [formRendered, setFormRendered] = useState(false);
+  const [actionDropDownShown, setActionDropDownShown] = useState(false);
+  const [checkedItems, setCheckedItems] = useState([]);
 
-  // ----- FORM HIDE/SHOW -----
+  // ----- HIDE/SHOW -----
   useEffect(() => {
     function hideForm(e) {
       const isAddProductButton =
@@ -15,15 +17,28 @@ function Products() {
         setFormRendered(false);
       }
     }
+    function hideActionDropDown(e) {
+      const isActionButtonGroup =
+        e.target === document.querySelector("#action-button-group") ||
+        document.querySelector("#action-button-group").contains(e.target);
+
+      if (actionDropDownShown && !isActionButtonGroup) {
+        setActionDropDownShown(false);
+      }
+    }
 
     if (formRendered) {
       document.addEventListener("click", hideForm); // only listen if form is rendered
     }
+    if (actionDropDownShown) {
+      document.addEventListener("click", hideActionDropDown); // only listen if options are rendered
+    }
 
     return () => {
-      document.removeEventListener("click", hideForm); // clean up listener
+      document.removeEventListener("click", hideForm);
+      document.removeEventListener("click", hideActionDropDown); // clean up listeners
     };
-  }, [formRendered]);
+  }, [formRendered, actionDropDownShown]);
 
   function addForm(e) {
     e.stopPropagation(); // Stop the click event from reaching the document when "Add Product" is clicked
@@ -61,6 +76,45 @@ function Products() {
     console.log("Data gathered from the form", formData);
   }
 
+  function showDropDown() {
+    setActionDropDownShown((prev) => !prev);
+  }
+
+  function handleOptionChange(e) {
+    const selectedOption = e.target.value;
+
+    if (selectedOption === "Delete") {
+      handleDelete();
+    }
+  }
+
+  function handleDelete() {
+    const itemsId = "______";
+
+    fetch(`___________/${itemsId}`, {
+      method: "DELETE",
+      Headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`Delete operation failed. ${res.status}`);
+        } else {
+          console.log("Deleted successfully.");
+        }
+      })
+      .catch((err) => {
+        console.log("Error while deleting: ", err);
+      });
+  }
+
+  function handleCheckedChange() {
+    // remove from/add to the checked array
+
+    setCheckedItems((prev) => !prev);
+  }
+
+  // function checkedAll() {}
+
   return (
     <>
       {/* ---------- import bar ---------- */}
@@ -74,9 +128,24 @@ function Products() {
         <table>
           <thead>
             <tr>
-              <th>square</th>
               <th>
-                <button>Action</button>
+                <input type="checkbox" />
+              </th>
+              <th>
+                <div id="action-button-group">
+                  <label id="action-button" onClick={showDropDown}>
+                    Action
+                  </label>
+                  {actionDropDownShown ? (
+                    <select onChange={handleOptionChange}>
+                      <option></option>
+                      <option>Change Category</option>
+                      <option>Delete</option>
+                    </select>
+                  ) : (
+                    ""
+                  )}
+                </div>
               </th>
               <th>Name</th>
               <th>In Stock</th>
@@ -89,7 +158,7 @@ function Products() {
             {formRendered === true ? (
               <tr id="form">
                 {/* ---------- form ---------- */}
-                <td>square</td>
+                <td>-</td>
                 <td>
                   <button>Add picture</button>
                 </td>
@@ -131,7 +200,15 @@ function Products() {
 
             {/* ---------- products ---------- */}
             <tr>
-              <td>square</td>
+              <td>
+                <div id="check-box-group">
+                  <input
+                    type="checkbox"
+                    checked={checkedItems.includes(1)}
+                    onChange={handleCheckedChange}
+                  />
+                </div>
+              </td>
               <td>picture</td>
               <td>Strawberry</td>
               <td>Available</td>
