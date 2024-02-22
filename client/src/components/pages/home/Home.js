8257a442abdecf9120760bfdeb2232455dafbf59
Svitlana <Svetlanalynne@gmail.com>
2024-02-21 23:23:28 -0400

    css in progress: logo small responsive added

--- client/src/components/pages/home/Home.js
+++ client/src/components/pages/home/Home.js
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 import Products from "../../products";
+import LogoImg from "../../../assets/logo-berries-small-bottom.png";
 
 function Home() {
   return (
     <div id="home-container">
-      <Link id="logout" to="/">
-        Logout
-      </Link>
-      <img id="small-logo" src="" alt="Berries Project Logo" />
+      <nav id="logo-group">
+        <Link id="logout" to="/">
+          Logout
+        </Link>
+        <img id="small-logo" src={LogoImg} alt="Berries Project Logo" />
+      </nav>
       <Products />
     </div>
   );
