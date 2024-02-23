d4db154e2224b0448a9afde5dee12936e558e1aa
Svitlana <Svetlanalynne@gmail.com>
2024-02-23 15:20:10 -0400

    css: icons added

--- client/src/components/import-Bar/ImportBar.js
+++ client/src/components/import-Bar/ImportBar.js
@@ -1,11 +1,24 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquarePlus, faFolderOpen } from "@fortawesome/free-solid-svg-icons";
+
 function ImportBar({ addForm, addImportOptions }) {
   return (
     <section id="import-bar-container">
       <button onClick={addForm} id="add-product-button">
         Add Product
+        <FontAwesomeIcon
+          className="custom-icon"
+          id="icon-plus"
+          icon={faSquarePlus}
+        />
       </button>
       <button id="import-button" onClick={addImportOptions}>
         Import
+        <FontAwesomeIcon
+          className="custom-icon"
+          id="icon-folder"
+          icon={faFolderOpen}
+        />
       </button>
     </section>
   );
